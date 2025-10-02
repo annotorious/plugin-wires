8d88db699b152ce947ebd5cf49eee968240aa45f
Rainer Simon <rainer@rainersimon.io>
2025-10-02 13:39:32 +0200

    Delayed layer init

--- packages/wires/src/osdWiresPlugin.ts
+++ packages/wires/src/osdWiresPlugin.ts
@@ -16,16 +16,30 @@ export const mountOSDPlugin = (
 
   let isEnabled = false;
 
-  const connectorLayer = new OSDWiresLayer({
-    target: viewer.element.querySelector('.openseadragon-canvas')!,
-    props: {
-      opts,
-      enabled: isEnabled,
-      graph,
-      state: anno.state as ImageAnnotatorState<ImageAnnotation>,
-      viewer
+  let connectorLayer: OSDWiresLayer
+
+  // Note that Annotorious OSD may take longer to initialize than the
+  // plugin! Ensure that everything is properly set up before we init the
+  // wires layer, because it will attach listeners to the annotation layer!
+  const mountOSDWiresLayer = (retries = 10) => {
+    const isReady = Boolean(viewer.element?.querySelector('.a9s-gl-canvas'));
+    if (isReady) {
+      connectorLayer = new OSDWiresLayer({
+        target: viewer.element.querySelector('.openseadragon-canvas')!,
+        props: {
+          opts,
+          enabled: isEnabled,
+          graph,
+          state: anno.state as ImageAnnotatorState<ImageAnnotation>,
+          viewer
+        }
+      })
+    } else if (retries > 0) {
+      setTimeout(() => mountOSDWiresLayer(retries - 1), 100)
+    } else {
+      throw new Error('Failed to initialize Annotorious Wires plugin: no annotation layer')
     }
-  });
+  }
 
   /** API **/
 
