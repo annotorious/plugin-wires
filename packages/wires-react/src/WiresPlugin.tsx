c0fa45c6f0fa780b850106578fe7915d0ed7779c
Rainer Simon <rainer@rainersimon.io>
2025-10-02 12:35:48 +0200

    Init props + reactive visibility prop

--- packages/wires-react/src/WiresPlugin.tsx
+++ packages/wires-react/src/WiresPlugin.tsx
@@ -1,6 +1,6 @@
 import { ReactNode, useCallback, useEffect, useState } from 'react';
 import { AnnotoriousImageAnnotator, AnnotoriousPlugin } from '@annotorious/react';
-import { WiresPluginInstance, mountPlugin as _mountPlugin } from '@annotorious/plugin-wires';
+import { WiresPluginInstance, WiresVisibility, mountPlugin as _mountPlugin } from '@annotorious/plugin-wires';
 import { WiresPluginProvider } from './WiresPluginProvider';
 
 interface WiresPluginProps {
@@ -9,18 +9,27 @@ interface WiresPluginProps {
 
   enabled?: boolean;
 
+  visibility?: WiresVisibility;
+
 }
 
 export const WiresPlugin = (props: WiresPluginProps) => {
 
   const [instance, setInstance] = useState<WiresPluginInstance>();
 
-  const mountPlugin = useCallback((anno: AnnotoriousImageAnnotator) => _mountPlugin(anno), []);
+  const mountPlugin = useCallback((anno: AnnotoriousImageAnnotator) => _mountPlugin(anno, {
+    showWires: props.visibility
+  }), []);
 
   useEffect(() => {
     if (instance)
       instance.setEnabled(props.enabled);
   }, [instance, props.enabled]);
+
+  useEffect(() => {
+    if (instance)
+      instance.setVisibility(props.visibility);
+  }, [instance, props.visibility]);
 
   return (
     <WiresPluginProvider instance={instance}>
